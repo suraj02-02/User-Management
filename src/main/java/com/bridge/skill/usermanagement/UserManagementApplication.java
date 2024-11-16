c4d36be9a52f51264395c2d87c7dad72f679adb1
surajyadav <surajyadav020406@gmail.com>
2024-11-16 15:47:50 +0530

    added package and refactored code

--- src/main/java/com/bridge/skill/usermanagement/UserManagementApplication.java
+++ src/main/java/com/bridge/skill/usermanagement/UserManagementApplication.java
@@ -1,13 +1,15 @@
 package com.bridge.skill.usermanagement;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class UserManagementApplication {
 
 	public static void main(String[] args) {
+	    log.info("Booting Up User Management Application......");
 		SpringApplication.run(UserManagementApplication.class, args);
 	}
-
 }
